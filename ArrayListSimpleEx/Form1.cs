0c29232e672dc56bf45578ccf7e58d851acf9493
ttj66 <ttj66@yahoo.fr>
2019-04-24 08:59:46 -0300

    done

--- ArrayListSimpleEx/Form1.cs
+++ ArrayListSimpleEx/Form1.cs
@@ -12,10 +12,50 @@ namespace ArrayListSimpleEx
 {
     public partial class Form1 : Form
     {
-        int i = 0;
+        String msg = "";
+        String insertString = "";
+        ArrayList wordlist = new ArrayList();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnShowMsg_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "";
+            for (int i = 0; i < wordlist.Count; i++)
+            {
+                lblMessage.Text = lblMessage.Text + " " + wordlist[i].ToString();
+            }
+        }
+
+        private void btnReverse_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = "";
+            for (int i = wordlist.Count - 1; i > -1; i--)
+            {
+                lblMessage.Text = lblMessage.Text + " " + wordlist[i].ToString();
+            }
+        }
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            insertString = txtSecondPos.Text;
+            wordlist.Insert(1, insertString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            msg = "I love programming so much";
+            string[] words = msg.Split(' ');
+            foreach (string word in words)
+            {
+                wordlist.Add(word);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            wordlist.RemoveAt(1);
         }
     }
 }
