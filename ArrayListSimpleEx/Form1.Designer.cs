e34b43c0a89bb7779280a9c6197cf0fe9012064e
ttj66 <ttj66@yahoo.fr>
2019-04-24 09:41:05 -0300

    last update

--- ArrayListSimpleEx/Form1.Designer.cs
+++ ArrayListSimpleEx/Form1.Designer.cs
@@ -39,71 +39,83 @@
             // btnShowMsg
             // 
             this.btnShowMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnShowMsg.Location = new System.Drawing.Point(29, 47);
+            this.btnShowMsg.Location = new System.Drawing.Point(39, 58);
+            this.btnShowMsg.Margin = new System.Windows.Forms.Padding(4);
             this.btnShowMsg.Name = "btnShowMsg";
-            this.btnShowMsg.Size = new System.Drawing.Size(188, 30);
+            this.btnShowMsg.Size = new System.Drawing.Size(251, 37);
             this.btnShowMsg.TabIndex = 0;
             this.btnShowMsg.Text = "Display Message";
             this.btnShowMsg.UseVisualStyleBackColor = true;
+            this.btnShowMsg.Click += new System.EventHandler(this.btnShowMsg_Click);
             // 
             // btnReverse
             // 
             this.btnReverse.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnReverse.Location = new System.Drawing.Point(223, 47);
+            this.btnReverse.Location = new System.Drawing.Point(297, 58);
+            this.btnReverse.Margin = new System.Windows.Forms.Padding(4);
             this.btnReverse.Name = "btnReverse";
-            this.btnReverse.Size = new System.Drawing.Size(188, 30);
+            this.btnReverse.Size = new System.Drawing.Size(251, 37);
             this.btnReverse.TabIndex = 1;
             this.btnReverse.Text = "Display Message In Reverse";
             this.btnReverse.UseVisualStyleBackColor = true;
+            this.btnReverse.Click += new System.EventHandler(this.btnReverse_Click);
             // 
             // lblMessage
             // 
             this.lblMessage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lblMessage.Location = new System.Drawing.Point(26, 106);
+            this.lblMessage.Location = new System.Drawing.Point(35, 130);
+            this.lblMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(385, 23);
+            this.lblMessage.Size = new System.Drawing.Size(513, 28);
             this.lblMessage.TabIndex = 2;
             // 
             // txtSecondPos
             // 
-            this.txtSecondPos.Location = new System.Drawing.Point(29, 156);
+            this.txtSecondPos.Location = new System.Drawing.Point(39, 192);
+            this.txtSecondPos.Margin = new System.Windows.Forms.Padding(4);
             this.txtSecondPos.Name = "txtSecondPos";
-            this.txtSecondPos.Size = new System.Drawing.Size(139, 20);
+            this.txtSecondPos.Size = new System.Drawing.Size(184, 22);
             this.txtSecondPos.TabIndex = 3;
             // 
             // btnAdd
             // 
             this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnAdd.Location = new System.Drawing.Point(174, 154);
+            this.btnAdd.Location = new System.Drawing.Point(232, 190);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(193, 23);
+            this.btnAdd.Size = new System.Drawing.Size(257, 28);
             this.btnAdd.TabIndex = 4;
             this.btnAdd.Text = "Add to Second Position";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(29, 206);
+            this.button1.Location = new System.Drawing.Point(39, 254);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(193, 23);
+            this.button1.Size = new System.Drawing.Size(257, 28);
             this.button1.TabIndex = 5;
             this.button1.Text = "Remove from Second Position";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(447, 255);
+            this.ClientSize = new System.Drawing.Size(596, 314);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtSecondPos);
             this.Controls.Add(this.lblMessage);
             this.Controls.Add(this.btnReverse);
             this.Controls.Add(this.btnShowMsg);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
